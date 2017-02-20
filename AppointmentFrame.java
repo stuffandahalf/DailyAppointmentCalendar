dff24660c86d9555471f0c78337dd1fd72ef0556
Stuff5 <freezedown@hotmail.com>
2017-02-20 18:50:00 -0500

    added methods to create gui compnents to AppointmentFrame

--- AppointmentFrame.java
+++ AppointmentFrame.java
@@ -2,9 +2,13 @@ import java.awt.BorderLayout;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 import java.util.Calendar;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JPanel;
 
 
 public class AppointmentFrame extends JFrame
@@ -14,17 +18,44 @@ public class AppointmentFrame extends JFrame
     
     private Calendar date;
     private SimpleDateFormat sdf;
+    private ArrayList<Appointment> appointments;
     
     private JLabel label;
+    private JTextArea textArea;
+    private JScrollPane scrollPane;
+    private JPanel controlPanel;
     
 	public AppointmentFrame()
     {
-        label = new JLabel();
         date = new GregorianCalendar();
         sdf = new SimpleDateFormat();
-        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        appointments = new ArrayList<Appointment>();
         
+        createLabel();
+        createTextArea();
+        createControlPanel();
+        
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
+    
+    private void createLabel()
+    {
+        label = new JLabel();
         add(label, BorderLayout.NORTH);
+    }
+    
+    private void createTextArea()
+    {
+        textArea = new JTextArea();
+        scrollPane = new JScrollPane(textArea);
+        add(scrollPane, BorderLayout.CENTER);
+    }
+    
+    private void createControlPanel()
+    {
+        controlPanel = new JPanel();
+        
+        add(controlPanel, BorderLayout.SOUTH);
     }
 }
 
